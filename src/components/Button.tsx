d938a31011a1431d9196d491d713bd4a85bff4bb
AudreyFelicio <audreyanwar@gmail.com>
2021-09-15 07:29:19 +0800

    chore: Generalize styled button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledButton = styled.button`
+const generateButtonColor = (props: any) => {
+  console.log(props);
+  if (props.primary) {
+    return props.theme.colors.emerald;
+  } else if (props.secondary) {
+    return props.theme.colors.blue;
+  } else if (props.danger) {
+    return props.theme.colors.terraCotta;
+  } else {
+    return props.theme.colors.emerald;
+  }
+};
+
+const StyledButton = styled.button<Props>`
   border-radius: 3px;
   border: 1px solid #42ad52;
-  background: #62c370;
+  background: ${(props) => generateButtonColor(props)};
   color: ${(props) => props.theme.colors.isabelline};
   font-family: ${(props) => props.theme.typeface};
   font-size: 17px;
@@ -19,12 +32,25 @@ const StyledButton = styled.button`
 
 interface Props {
   onClick: React.MouseEventHandler;
+  primary?: boolean;
+  secondary?: boolean;
+  danger?: boolean;
 }
 
 const Button: React.FC<Props> = function (props) {
-  const { onClick, children } = props;
+  console.log(props);
+  const { onClick, children, primary, secondary, danger } = props;
 
-  return <StyledButton onClick={onClick}>{children}</StyledButton>;
+  return (
+    <StyledButton
+      onClick={onClick}
+      primary={primary}
+      secondary={secondary}
+      danger={danger}
+    >
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;
