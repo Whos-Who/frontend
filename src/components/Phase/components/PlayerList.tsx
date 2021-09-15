513a113b21be4714a6803a38e8568770abf1d4c5
Chester How <chesterhow@gmail.com>
2021-09-15 18:56:03 +0800

    style: style player list

--- src/components/Phase/components/PlayerList.tsx
+++ src/components/Phase/components/PlayerList.tsx
@@ -4,16 +4,19 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 30px;
+  padding: 15px 30px;
+  text-align: center;
 `;
 
 const TotalPlayers = styled.h3`
-  margin: 0 auto;
+  margin: 0 0 10px;
+  font-size: ${(props) => props.theme.fontSizes.xl};
   color: ${(props) => props.theme.colors.black};
 `;
 
-const DisplayPlayer = styled.h4`
-  margin: 1rem auto;
+const DisplayPlayer = styled.h3`
+  margin: 10px 0 0;
+  font-size: ${(props) => props.theme.fontSizes.xl};
   color: ${(props) => props.theme.colors.blue};
 `;
 
