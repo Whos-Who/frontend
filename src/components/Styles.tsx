daa13598bfeedaa71ba8a7ef1986096e37eee566
wenhao <wenhaogoh1998@gmail.com>
2021-09-20 02:01:55 +0800

    style: Animate error message

--- src/components/Styles.tsx
+++ src/components/Styles.tsx
@@ -20,6 +20,8 @@ export const StyledInput = styled.input.attrs({
   font-size: ${(props) => props.theme.fontSizes.md};
   font-weight: ${(props) => props.theme.fontWeights.bold};
   text-align: center;
+  animation: ${(props) =>
+    props.$error && "shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both"};
 
   ::placeholder {
     color: ${(props) => props.theme.colors.grayLight};
@@ -34,6 +36,29 @@ export const StyledInput = styled.input.attrs({
     border: 3px solid
       ${(props) =>
         props.$error ? props.theme.colors.rose : props.theme.colors.blue};
+  }
+
+  @keyframes shake {
+    10%,
+    90% {
+      transform: translate3d(-1px, 0, 0);
+    }
+
+    20%,
+    80% {
+      transform: translate3d(2px, 0, 0);
+    }
+
+    30%,
+    50%,
+    70% {
+      transform: translate3d(-4px, 0, 0);
+    }
+
+    40%,
+    60% {
+      transform: translate3d(4px, 0, 0);
+    }
   }
 `;
 
