dd4ff2f2528f384a672436aaeebe1cb7b16cc275
AudreyFelicio <audreyanwar@gmail.com>
2021-09-20 23:16:15 +0800

    chore: Add deck card

--- src/components/DecksList.tsx
+++ src/components/DecksList.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useHistory } from "react-router";
 import styled from "styled-components";
+
+import Theme from "../styles/theme";
+import DeckCard from "./DeckCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -12,12 +16,34 @@ interface Props {
 }
 
 const DecksList: React.FC<Props> = (props) => {
+  const history = useHistory();
   const { decks } = props;
+
+  const generateRandomColor = () => {
+    const deckColors = [
+      Theme.colors.blue,
+      Theme.colors.rose,
+      Theme.colors.emerald,
+    ];
+
+    return deckColors[Math.floor(Math.random() * deckColors.length)];
+  };
+
+  const navigateToDeckView = (id: string) => () => {
+    history.push(`/decks/${id}`);
+  };
 
   return (
     <Wrapper>
-      {decks.map((entry) => {
-        return <h1 key={entry.id}>{entry.title}</h1>;
+      {decks.map((deck) => {
+        return (
+          <DeckCard
+            key={deck.id}
+            title={deck.title}
+            color={generateRandomColor()}
+            navigateToDeckView={navigateToDeckView(deck.id)}
+          />
+        );
       })}
     </Wrapper>
   );
