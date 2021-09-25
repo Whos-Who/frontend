e23de873333bb3614c48bf1a04067a949556838f
Chester How <chesterhow@gmail.com>
2021-09-25 09:16:36 +0800

    fix: linting errors

--- src/components/Phase/TurnRevealPhase/components/AnswerValidity.tsx
+++ src/components/Phase/TurnRevealPhase/components/AnswerValidity.tsx
@@ -85,7 +85,9 @@ const AnswerValidity: React.FC<Props> = function (props) {
 
   if (!alreadyGuessed) {
     scoreIncrement = Object.values(players)
-      .map((player): number => (player.currAnswer.isGuessed || player.currAnswer.value == "" ? 0 : 1))
+      .map((player): number =>
+        player.currAnswer.isGuessed || player.currAnswer.value == "" ? 0 : 1
+      )
       .reduce((a, b) => a + b, 1);
   }
 
